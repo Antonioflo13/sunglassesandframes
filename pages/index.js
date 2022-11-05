0155a4828322d50dde51011dbeb4a3ffb179b4d3
antonioflore <floreantonio@hotmail.it>
2022-11-05 13:43:23 +0100

    add: cookie

--- pages/index.js
+++ pages/index.js
@@ -60,7 +60,10 @@ const IndexPage = ({ articles }) => {
                 "onReady": function() {
                     console.log('Cookie banner is shown');
                     var banner = document.getElementById('iubenda-cs-banner');
-                    console.log(banner);
+                  
+                    if (banner && localStorage.getItem("cookie-accepted")) {
+                      banner = null;
+                    }
                     if (banner && !localStorage.getItem("cookie-accepted")) {
                       bannerHTML = banner.innerHTML;
                     }
