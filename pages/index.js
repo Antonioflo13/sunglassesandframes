c5a9a6ff9f6e875a684cb2dd56245018b7ad9d19
antonioflore <floreantonio@hotmail.it>
2022-11-05 12:03:22 +0100

    add: cookie

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 //REACT
-import React, { useEffect, useState } from "react";
-//HOOKS
-import cookie from "../hooks/cookie";
+import React, { useState } from "react";
 //API
 import getAllArticles from "../api/articles";
 //COMPONENTS
@@ -12,6 +10,7 @@ import SliderArticles from "../components/slider-articles";
 import AnimatedPage from "../components/animated-page";
 import Layout from "../components/layout";
 import Head from "next/head";
+import Script from "next/script";
 import { useSelector } from "react-redux";
 
 const IndexPage = ({ articles }) => {
@@ -21,13 +20,34 @@ const IndexPage = ({ articles }) => {
   const [show, setShown] = useState(false);
   articles = articles.data.allArticles;
   let selectSingleIcon;
-  //EFFECT
-  useEffect(() => {
-    cookie(language);
-  }, [language]);
 
   return (
     <>
+      <Script
+        type="text/javascript"
+        src="https://cdn.iubenda.com/cs/iubenda_cs.js"
+        async
+      />
+      <Script>{`var _iub = _iub || [];
+        _iub.csConfiguration = {
+        "lang":"${language}","siteId":2172061,"cookiePolicyId":22164738, 
+          "banner": { 
+            "acceptButtonDisplay":true,
+            "customizeButtonDisplay":true,
+            "acceptButtonColor":"#800000",
+            "acceptButtonCaptionColor":"white",
+            "customizeButtonColor":"#212121",
+            "customizeButtonCaptionColor":"white",
+            "rejectButtonColor":"#0073CE",
+            "rejectButtonCaptionColor":"white",
+            "position":"float-bottom-center",
+            "textColor":"white",
+            "backgroundColor":
+            "#000001",
+            "fontSizeBody":12 
+          }
+        };`}</Script>
+
       <Head>
         <title>Indice</title>
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
