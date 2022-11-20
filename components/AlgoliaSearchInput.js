80042ed25b989b1837b3e78f33d10086411ceaa4
Abanoub Zaky <zaky.abanoub@gmail.com>
2022-11-20 16:58:27 +0100

    edit algliasearch

--- components/AlgoliaSearchInput.js
+++ components/AlgoliaSearchInput.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBox = ({ currentRefinement, isSearchStalled, refine }) => (
-  <label class="relative text-gray-400 focus-within:text-gray-600 block w-[100%] md:w-[70%] m-auto">
+  <label className="relative text-gray-400 focus-within:text-gray-600 block w-[100%] md:w-[70%] m-auto">
     <FontAwesomeIcon
       className="pointer-events-none absolute top-1/2 transform -translate-y-1/2 left-3 w-[1.7rem] h-[1.7rem]"
       icon={faSearch}
