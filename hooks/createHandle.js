8452e00982de72ae956319ea2615b1a4f157f067
antonioflore <floreantonio@hotmail.it>
2022-12-04 20:02:38 +0100

    [feature/handle] : complete

--- hooks/createHandle.js
+++ hooks/createHandle.js
@@ -1,5 +1,11 @@
 const createHandle = title => {
-  return title.toLowerCase().replaceAll(" ", "-");
+  const specialCharacter = title.match("[^A-Za-z0-9/ +/]");
+  return title
+    .toLowerCase()
+    .replaceAll(specialCharacter && specialCharacter[0], "")
+    .replaceAll(" ", "-")
+    .replace("-", "")
+    .trim();
 };
 
 export default createHandle;
