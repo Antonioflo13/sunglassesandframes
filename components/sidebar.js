aa43a3714e010629cd057ace1f4f95d3ef60c01b
GiovanniP <giovannip@MBP003.local>
2022-12-07 15:10:30 +0100

    add info

--- components/sidebar.js
+++ components/sidebar.js
@@ -114,7 +114,26 @@ const Sidebar = ({
             </div>
           </div>
         </div>
-        <div className="line" />
+
+        <div className="containerLineInfo">
+          <div className="line" />
+          <div className="containerInfo mt-5">
+            <p>Need help?</p>
+            <div className="info">
+              <p>Whatsapp</p>
+              <p>+39 339 46 44 407</p>
+              <p>Available Monday to Friday, 09:00 - 18:00 EST</p>
+            </div>
+            <div className="info">
+              <p>E-mail us</p>
+              <p>info@sunglassesandframes.com</p>
+            </div>
+            <div className="info">
+              <p>Assistance Center</p>
+              <p>Anything you need to know is here</p>
+            </div>
+          </div>
+        </div>
       </motion.div>
       {show && (
         <ModalsIcons selectSingleIcon={selectSingleIcon} setShown={setShown} />
@@ -176,6 +195,25 @@ const Sidebar = ({
                       </div>
                     </div>
                   ))}
+                </div>
+              </div>
+            </div>
+            <div className="containerLineInfo">
+              <div className="line" />
+              <div className="containerInfo mt-5">
+                <p>Need help?</p>
+                <div className="info">
+                  <p>Whatsapp</p>
+                  <p>+39 339 46 44 407</p>
+                  <p>Available Monday to Friday, 09:00 - 18:00 EST</p>
+                </div>
+                <div className="info">
+                  <p>E-mail us</p>
+                  <p>info@sunglassesandframes.com</p>
+                </div>
+                <div className="info">
+                  <p>Assistance Center</p>
+                  <p>Anything you need to know is here</p>
                 </div>
               </div>
             </div>
@@ -241,6 +279,25 @@ const Sidebar = ({
                 </div>
               </div>
             </div>
+            <div className="containerLineInfo">
+              <div className="line" />
+              <div className="containerInfo mt-5">
+                <p>Need help?</p>
+                <div className="info">
+                  <p>Whatsapp</p>
+                  <p>+39 339 46 44 407</p>
+                  <p>Available Monday to Friday, 09:00 - 18:00 EST</p>
+                </div>
+                <div className="info">
+                  <p>E-mail us</p>
+                  <p>info@sunglassesandframes.com</p>
+                </div>
+                <div className="info">
+                  <p>Assistance Center</p>
+                  <p>Anything you need to know is here</p>
+                </div>
+              </div>
+            </div>
           </motion.div>
         )}
       </AnimatePresence>
@@ -280,12 +337,24 @@ const Sidebar = ({
             right: 1.5rem;
           }
 
+          .containerLineInfo {
+            position: absolute;
+            top: 35%;
+            width: 100%;
+          }
           .line {
             width: 100%;
             height: 1px;
-            position: absolute;
-            top: 40%;
             background: #c0c0c0;
+          }
+
+          .containerInfo {
+            padding-left: 1.25rem;
+            padding-right: 1.25rem;
+          }
+
+          .info {
+            margin-top: 25px;
           }
         `}
       </style>
